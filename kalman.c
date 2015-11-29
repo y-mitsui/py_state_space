7402617e5e8e88f20d6bceca796d9ca879d6e9ec
Y-Mitsui <dyfomg@gmail.com>
2015-11-30 07:26:56 +0900

    commit

---
+++ kalman.c
@@ -0,0 +1,311 @@
+#include <stdio.h>
+#include <gsl/gsl_rng.h>
+#include <gsl/gsl_randist.h>
+#include <gsl/gsl_blas.h>
+#include <gsl/gsl_vector.h>
+#include <gsl/gsl_matrix.h>
+#include <gsl/gsl_linalg.h>
+#include <gsl/gsl_blas.h>
+#include <Python.h>
+
+#define dmalloc(size) debug_malloc(size,__FILE__,__LINE__)
+
+gsl_vector *gsl_vector_clone(const gsl_vector *src){
+	gsl_vector *r=gsl_vector_alloc(src->size);
+	gsl_vector_memcpy(r,src);
+	return r;
+}
+gsl_matrix *gsl_matrix_clone(const gsl_matrix *src){
+	gsl_matrix *r=gsl_matrix_alloc(src->size1,src->size2);
+	gsl_matrix_memcpy(r,src);
+	return r;
+}
+void gsl_matrix_mul_constant(gsl_matrix *a,const double x){
+	int i,j;
+	for(i=0;i<a->size1;i++){
+		for(j=0;j<a->size2;j++){
+			gsl_matrix_set(a,i,j,gsl_matrix_get(a,i,j)*x);
+		}
+	}
+}
+
+gsl_matrix *gsl_inverse(gsl_matrix *m){
+	int s=0;
+	gsl_permutation * p = gsl_permutation_alloc (m->size1);
+	gsl_matrix *inv=gsl_matrix_clone(m);
+	gsl_matrix *r=gsl_matrix_alloc(inv->size1,inv->size2);
+	gsl_linalg_LU_decomp(inv,p,&s);
+	gsl_linalg_LU_invert(inv,p,r);
+	gsl_matrix_free(inv);
+	gsl_permutation_free(p);
+	return r;
+}
+
+typedef struct {
+    gsl_vector **state_filter, **state_predictor;
+    gsl_matrix **covariance_filter, **covariance_predictor;
+    gsl_vector **y_forecast;
+    gsl_matrix **cov_forecast;
+    gsl_matrix *matrix_km[1];
+    gsl_matrix *matrix_lk[1];
+    gsl_matrix *matrix_ll[1];
+    gsl_matrix *matrix_kl[1];
+    
+    gsl_vector *vector_l[1];
+    gsl_vector *vector_k[1];
+    
+    gsl_matrix *tmp_matrix[5];
+    gsl_vector *tmp_vector[2];
+    gsl_matrix *gain;
+    int state_dimention;
+    int y_dimention;
+}Kalman;
+
+void* debug_malloc(const size_t size, const char *file,const int line){
+    void *r = malloc(size);
+    if (r == NULL) {
+        fprintf(stderr, "out of memory %s:%d", file, line);
+    }
+    return r;
+}
+
+Kalman *kalmanInit(int n_y, int y_dimention, int state_dimention,int m){
+    Kalman *kalman = dmalloc(sizeof(Kalman));
+    int i;
+    
+    kalman->state_filter = dmalloc(sizeof(gsl_vector *) * n_y);
+    kalman->covariance_filter = dmalloc(sizeof(gsl_matrix *) * n_y);
+    kalman->state_predictor = dmalloc(sizeof(gsl_vector *) * n_y);
+    kalman->covariance_predictor = dmalloc(sizeof(gsl_matrix *) * n_y);
+    kalman->y_forecast = dmalloc(sizeof(gsl_vector *) * n_y);
+    kalman->cov_forecast = dmalloc(sizeof(gsl_matrix *) * n_y);
+    
+    for(i=0;i<n_y;i++){
+        kalman->state_filter[i] = gsl_vector_alloc(state_dimention);
+        kalman->state_predictor[i] = gsl_vector_alloc(state_dimention);
+        kalman->covariance_filter[i] = gsl_matrix_alloc(state_dimention,state_dimention);
+        kalman->covariance_predictor[i] = gsl_matrix_alloc(state_dimention,state_dimention);
+        kalman->y_forecast[i] = gsl_vector_alloc(y_dimention);
+        kalman->cov_forecast[i] = gsl_matrix_alloc(y_dimention,y_dimention);
+    }
+    kalman->matrix_km[0]=gsl_matrix_alloc(state_dimention, m);
+    kalman->matrix_lk[0]=gsl_matrix_alloc(y_dimention, state_dimention);
+    kalman->matrix_ll[0]=gsl_matrix_alloc(y_dimention, y_dimention);
+    kalman->matrix_kl[0]=gsl_matrix_alloc(state_dimention, y_dimention);
+    
+    kalman->vector_l[0] = gsl_vector_alloc(y_dimention);
+    kalman->vector_k[0] = gsl_vector_alloc(state_dimention);
+    
+    kalman->tmp_matrix[0]=gsl_matrix_alloc(state_dimention,state_dimention);
+    kalman->tmp_matrix[1]=gsl_matrix_alloc(state_dimention,state_dimention);
+    kalman->tmp_matrix[2]=gsl_matrix_alloc(state_dimention,state_dimention);
+    kalman->tmp_matrix[3]=gsl_matrix_alloc(state_dimention,state_dimention);
+    kalman->tmp_matrix[4]=gsl_matrix_alloc(state_dimention,state_dimention);
+    kalman->tmp_vector[0]=gsl_vector_alloc(state_dimention);
+    kalman->tmp_vector[1]=gsl_vector_alloc(state_dimention);
+    
+    kalman->gain=gsl_matrix_alloc(state_dimention,y_dimention);
+    kalman->state_dimention = state_dimention;
+    kalman->y_dimention = y_dimention;
+    return kalman;
+}
+
+void kalmanPredictionStep(Kalman *kalman,gsl_vector *state_filter, gsl_matrix *covariance_filter,gsl_vector *state_predictor, gsl_matrix *covariance_predictor,
+                          gsl_matrix *A, gsl_matrix *constant_covariance){
+                          
+    gsl_blas_dgemv(CblasNoTrans, 1.0, A, state_filter, 0.0, state_predictor);
+    gsl_blas_dgemm(CblasNoTrans, CblasNoTrans, 1.0, A, covariance_filter, 0.0, kalman->tmp_matrix[0]);
+    gsl_blas_dgemm(CblasNoTrans, CblasTrans , 1.0, kalman->tmp_matrix[0], A, 0.0, covariance_predictor);
+    
+    gsl_matrix_add(covariance_predictor,constant_covariance);
+}
+
+void kalmanFilteringStep(Kalman *kalman,gsl_vector *state_predictor, gsl_matrix *covariance_predictor,gsl_vector *state_filter, gsl_matrix *covariance_filter,
+                         gsl_vector *each_y, gsl_matrix *c, gsl_matrix *sigma_w){
+
+    gsl_blas_dgemm(CblasTrans, CblasNoTrans, 1.0, c, covariance_predictor, 0.0, kalman->matrix_lk[0]);
+    gsl_blas_dgemm(CblasNoTrans, CblasNoTrans, 1.0, kalman->matrix_lk[0], c, 0.0, kalman->matrix_ll[0]);
+    gsl_matrix_add(kalman->matrix_ll[0], sigma_w);
+    gsl_matrix *tmp = gsl_inverse(kalman->matrix_ll[0]);
+    gsl_blas_dgemm(CblasNoTrans, CblasNoTrans, 1.0, covariance_predictor, c, 0.0, kalman->matrix_kl[0]);
+    gsl_blas_dgemm(CblasNoTrans, CblasNoTrans, 1.0, kalman->matrix_kl[0], tmp, 0.0, kalman->gain);
+    gsl_blas_dgemv(CblasTrans , 1.0, c, state_predictor, 0.0, kalman->vector_l[0]);
+    gsl_vector *v =  gsl_vector_clone(each_y);
+    gsl_vector_sub(v, kalman->vector_l[0]);
+    gsl_blas_dgemv(CblasNoTrans, 1.0, kalman->gain, kalman->vector_l[0], 0.0, kalman->vector_k[0]);
+    gsl_vector_memcpy(state_filter,state_predictor);
+    gsl_vector_add(state_filter, kalman->vector_k[0]);
+    gsl_blas_dgemm(CblasNoTrans, CblasTrans, 1.0, kalman->gain, c, 0.0, kalman->tmp_matrix[4]);
+    gsl_matrix *tmp2 = gsl_matrix_alloc(kalman->state_dimention, kalman->state_dimention);
+    gsl_matrix_set_identity(tmp2);
+    gsl_matrix_sub(tmp2,kalman->tmp_matrix[4]);
+    gsl_blas_dgemm(CblasNoTrans, CblasNoTrans, 1.0, tmp2, covariance_predictor, 0.0, covariance_filter);
+}
+
+void kalmanFilter(Kalman *kalman,gsl_vector **y, int n_y, gsl_matrix *A, gsl_matrix *b, gsl_matrix *c, gsl_matrix *sigma_Q, gsl_matrix *sigma_w, gsl_vector *x0){
+    int i;
+    
+    gsl_matrix *covariance_filter0 = gsl_matrix_alloc(kalman->state_dimention, kalman->state_dimention);
+    gsl_matrix_set_identity(covariance_filter0);
+    gsl_matrix_mul_constant(covariance_filter0, gsl_matrix_get(sigma_w, 0, 0));
+    gsl_matrix *constant_covariance = gsl_matrix_alloc(kalman->state_dimention, kalman->state_dimention);
+    gsl_blas_dgemm(CblasNoTrans, CblasNoTrans, 1.0, b, sigma_Q, 0.0, kalman->matrix_km[0]);
+    gsl_blas_dgemm(CblasNoTrans, CblasTrans, 1.0, kalman->matrix_km[0], b, 0.0, constant_covariance);
+    
+    kalmanPredictionStep(kalman, x0, covariance_filter0, kalman->state_predictor[0], kalman->covariance_predictor[0], A, constant_covariance);
+    gsl_blas_dgemv(CblasTrans, 1.0, c, kalman->state_predictor[0], 0.0, kalman->y_forecast[0]);
+    gsl_matrix *tmp=gsl_matrix_alloc(kalman->y_dimention,kalman->state_dimention);
+    gsl_blas_dgemm(CblasTrans, CblasNoTrans, 1.0, c, kalman->covariance_predictor[0], 0.0, tmp);
+    gsl_blas_dgemm(CblasNoTrans, CblasNoTrans, 1.0, tmp, c, 0.0, kalman->cov_forecast[0]);
+    gsl_matrix_add(kalman->cov_forecast[0],sigma_w);
+    kalmanFilteringStep(kalman, kalman->state_predictor[0], kalman->covariance_predictor[0], kalman->state_filter[0], kalman->covariance_filter[0], y[0], c, sigma_w);
+    for(i=1;i < n_y;i++) {
+        kalmanPredictionStep(kalman, kalman->state_filter[i-1], kalman->covariance_filter[i-1], kalman->state_predictor[i], kalman->covariance_predictor[i], A, constant_covariance);
+        printf("kalman->state_predictor[%d]:%lf\n",i,gsl_vector_get(kalman->state_predictor[i],0));
+        gsl_blas_dgemv(CblasTrans, 1.0, c, kalman->state_predictor[i], 0.0, kalman->y_forecast[i]);
+        gsl_blas_dgemm(CblasTrans, CblasNoTrans, 1.0, c, kalman->covariance_predictor[i], 0.0, kalman->matrix_lk[0]);
+        gsl_blas_dgemm(CblasNoTrans, CblasNoTrans, 1.0, kalman->matrix_lk[0], c, 0.0, kalman->cov_forecast[i]);
+        gsl_matrix_add(kalman->cov_forecast[i],sigma_w);
+        kalmanFilteringStep(kalman, kalman->state_predictor[i], kalman->covariance_predictor[i], kalman->state_filter[i], kalman->covariance_filter[i], y[i], c, sigma_w);
+    }
+    
+}
+static PyObject *kalmanFilterInitInterface(PyObject *self, PyObject *args){
+    int n_y, state_dimention, y_dimention, m;
+    
+    if (! PyArg_ParseTuple( args, "iiii", &n_y, &y_dimention, &state_dimention, &m)) return NULL;
+    Kalman *kalman = kalmanInit(n_y, y_dimention, state_dimention, m);
+    return Py_BuildValue("O", PyCapsule_New(kalman,NULL,NULL));
+}
+gsl_matrix *readArg(PyObject *arg){
+    int num[2],i,j;
+    gsl_matrix *mat;
+    PyObject *row,*col;
+    
+    if((num[0] = PyList_Size(arg)) < 0) return NULL;
+    if((row = PyList_GetItem(arg, 0))==NULL) return NULL;
+    if((num[1] = PyList_Size(row)) < 0) return NULL;
+    mat = gsl_matrix_alloc(num[0],num[1]);
+    for (i=0; i<num[0]; i++){
+        row = PyList_GetItem(arg, i);
+        for (j=0; j<num[1]; j++){
+            col = PyList_GetItem(row, j);
+            double tmp=PyFloat_AsDouble(col);
+            gsl_matrix_set(mat,i,j,tmp);
+        }
+    }
+    return mat;
+}
+
+gsl_vector *readArgVec(PyObject *arg){
+    int num,i;
+    gsl_vector *vec;
+    PyObject *row;
+    
+    if((num = PyList_Size(arg)) < 0) return NULL;
+    
+    vec = gsl_vector_alloc(num);
+    for (i=0; i<num; i++){
+        row = PyList_GetItem(arg, i);
+        double tmp=PyFloat_AsDouble(row);
+        gsl_vector_set(vec,i,tmp);
+    }
+    return vec;
+}
+
+gsl_vector **readArgVecs(PyObject *arg){
+    int num[2],i;
+    gsl_vector **vecs;
+    PyObject *row;
+    
+    if((num[0] = PyList_Size(arg)) < 0) return NULL;
+    if((row = PyList_GetItem(arg, 0))==NULL) return NULL;
+    if((num[1] = PyList_Size(row)) < 0) return NULL;
+    vecs = dmalloc(sizeof(gsl_vector *) * num[0]);
+    for (i=0; i<num[0]; i++){
+        row = PyList_GetItem(arg, i);
+        vecs[i] = readArgVec(row);
+    }
+    return vecs;
+}
+
+
+PyListObject *convertPyObjectFromMatries(gsl_matrix **mats,int n){
+    int i,j,k;
+    puts("f1");
+    PyListObject *list = (PyListObject *) PyList_New(n);
+    for(i=0; i < n; i++){
+        PyListObject *list2 = (PyListObject *) PyList_New(mats[i]->size1);
+        for(j=0; j<mats[i]->size1; j++){
+            PyListObject *list3 = (PyListObject *) PyList_New(mats[i]->size2);
+            for(k=0; k<mats[i]->size2; k++){
+                PyList_SET_ITEM(list3, k, Py_BuildValue("f", gsl_matrix_get(mats[i], j, k)));
+            }
+            PyList_SET_ITEM(list2,j,Py_BuildValue("O",list3));
+        }
+        PyList_SET_ITEM(list,i,Py_BuildValue("O",list2));
+    }
+    return list;
+}
+PyListObject *convertPyObjectFromVectors(gsl_vector **vecs,int n){
+    int i,j;
+    PyListObject *list = (PyListObject *) PyList_New(n);
+    for(i=0; i < n; i++){
+        PyListObject *list2 = (PyListObject *) PyList_New(vecs[i]->size);
+        for(j=0; j<vecs[i]->size; j++){
+            PyList_SET_ITEM(list2, j, Py_BuildValue("f", gsl_vector_get(vecs[i], j)));
+        }
+        PyList_SET_ITEM(list,i,Py_BuildValue("O",list2));
+    }
+    return list;
+}
+static PyObject *kalmanFilterInterface(PyObject *self, PyObject *args){
+    Kalman *kalman;
+    PyObject *arg_kalman,*arg_A, *arg_b, *arg_c, *arg_sigma_Q, *arg_sigma_w, *vecY, *arg_x0;
+    gsl_matrix *A,*b,*c, *sigma_Q, *sigma_w;
+    gsl_vector **y,*x0;
+    int n_y;
+    
+    if (! PyArg_ParseTuple( args, "OOOOOOOO", &arg_kalman, &vecY, &arg_A, &arg_b, &arg_c, &arg_sigma_Q, &arg_sigma_w, &arg_x0)) return NULL;
+    puts("a");
+    kalman=PyCapsule_GetPointer(arg_kalman,NULL);
+    
+    if((n_y = PyList_Size(vecY)) < 0) return NULL;
+    puts("b");
+    y = readArgVecs(vecY);
+    A = readArg(arg_A);
+    b = readArg(arg_b);
+    c = readArg(arg_c);
+    puts("c");
+    sigma_Q = readArg(arg_sigma_Q);
+    sigma_w = readArg(arg_sigma_w);
+    x0 = readArgVec(arg_x0);
+    puts("d");
+    kalmanFilter(kalman,y,n_y,A,b,c,sigma_Q,sigma_w,x0);
+    puts("e");
+    PyListObject *list = (PyListObject *) PyList_New(6);
+    PyList_SET_ITEM(list,0,Py_BuildValue("O",convertPyObjectFromVectors(kalman->state_predictor, n_y)));
+    puts("f");
+    PyList_SET_ITEM(list,1,Py_BuildValue("O",convertPyObjectFromMatries(kalman->covariance_predictor, n_y)));
+    puts("g");
+    PyList_SET_ITEM(list,2,Py_BuildValue("O",convertPyObjectFromVectors(kalman->state_filter, n_y)));
+    puts("h");
+    PyList_SET_ITEM(list,3,Py_BuildValue("O",convertPyObjectFromMatries(kalman->covariance_filter, n_y)));
+    puts("i");
+    PyList_SET_ITEM(list,4,Py_BuildValue("O",convertPyObjectFromVectors(kalman->y_forecast, n_y)));
+    puts("j");
+    PyList_SET_ITEM(list,5,Py_BuildValue("O",convertPyObjectFromMatries(kalman->cov_forecast, n_y)));
+    puts("k");
+    return Py_BuildValue("O", list);
+}
+static PyMethodDef kalmanmethods[] = {
+    {"filter", kalmanFilterInterface, METH_VARARGS},
+    {"init", kalmanFilterInitInterface, METH_VARARGS},
+    {NULL},
+};
+
+void initkalman(void)
+{
+    Py_InitModule("kalman", kalmanmethods);
+}
+
--- py_state_space.py
+++ py_state_space.py
@@ -5,9 +5,27 @@ import pandas as pd
 from  scipy import optimize
 import math
 import matplotlib.pyplot as plt
+from scipy.spatial.distance import squareform
+import sys
+from statsmodels.tsa.stattools import acf
+import kalman
+import copy
+
+def getModel(trend=1,period=7,ar=3):
+    A = np.matrix(np.zeros((period+1,period+1)))
+    A[2,2:] = np.ones(period - 1) * -1
+    A[3:,2:-1] = np.diag(np.ones(period-2))
+    A[0:2,0:2] = np.matrix([[2,-1],[1,0]])
+    b = np.matrix(np.zeros((period-1+2,2)))
+    b[0,0] = 1
+    b[2,1] = 1
+    c = np.matrix(np.zeros((period-1+2,1)))
+    c[0,0] = 1
+    c[2,0] = 1
+    return {'A':A,'b':b,'c':c}
 
 u""" 状態空間モデルを解析 """
-
+        
 class PyStateSpace:
     u"""
     [CLASSES] 以下の状態空間モデルを取り扱うクラス
@@ -19,7 +37,7 @@ class PyStateSpace:
     b: m X 1 numpy.matrix class
     c: l X 1 numpy.matrix class
     """
-    def __init__(self, A, b, c, debug=False):
+    def __init__(self, model, sigma=None, debug=False):
         u""" [CLASSES] 
         Keyword arguments:
         A -- numpy.matrix 
@@ -27,75 +45,152 @@ class PyStateSpace:
         c -- numpy.matrix
         sigma -- numpy.ndarray
         """
-        self.A = A
-        self.b = b
-        self.c = c
-
-    def filter(self, y, sigma_v, sigma_w ,x0):
+        self.A = model['A']
+        self.b = model['b']
+        self.c = model['c']
+        self.sigma = sigma
+    def filter(self, y, sigma_Q, sigma_w ,x0):
         u""" [CLASSES] カルマンフィルタによる状態平均ベクトル・分散共分散の推定
         Return value:
         """
         
         n_dimention = x0.shape[0]
+        self.n_sample = y.shape[0]
         self.state_filter, self.state_predictor = [], []
         self.covariance_filter, self.covariance_predictor = [], []
         self.y_forecast, self.cov_forecast = [], []
         
         self.state_filter.append(x0)
-        self.covariance_filter.append(np.ones((n_dimention, n_dimention)) * sigma_w)
-        
+        print sigma_w
+        self.covariance_filter.append(np.eye(n_dimention) * sigma_w[0,0])
+        constant_coveriance = self.b * sigma_Q * self.b.T
         for each_y in y:
-            #1期先予測
+            #1期先予測  
             self.state_predictor.append(self.A * self.state_filter[-1])
-            self.covariance_predictor.append(self.A * self.covariance_filter[-1] * self.A.T + sigma_v * (self.b * self.b.T))
+            self.covariance_predictor.append(self.A * self.covariance_filter[-1] * self.A.T + constant_coveriance)
+            """if any(np.diag(self.covariance_predictor[-1]) < 0.0):
+                a=self.A * self.covariance_filter[-1] * self.A.T
+                b= self.b * sigma_Q * self.b.T
+                print "----------------- failed -------------------"
+                print "np.diag(a):{}".format(np.diag(a))
+                print "np.diag(b):{}".format(np.diag(b))
+                print "np.diag(self.covariance_filter[-1]):{}".format(np.diag(self.covariance_filter[-1]))
+                print "gain * self.c.T :{}".format(gain * self.c.T)
+                print "np.diag(gain * self.c.T):{}".format(np.diag(gain * self.c.T))
+                print "index:%d"%(i)
+                print "fail sigma_Q:{}".format(sigma_Q)
+                print "fail sigma_w:{}".format(sigma_w)
+                raise Exception('covariance included nagative values')
+            self.covariance_predictor[-1].I"""
+            
             self.y_forecast.append(self.c.T * self.state_predictor[-1])
             self.cov_forecast.append(self.c.T * self.covariance_predictor[-1] * self.c + sigma_w)
+            
             #フィルタリング
-            gain = self.covariance_predictor[-1] * self.c / ( self.c.T * self.covariance_predictor[-1] * self.c  + sigma_w)
-            self.state_filter.append(self.state_predictor[-1] + gain * ( each_y - self.c.T * self.state_predictor[-1]))
-            self.covariance_filter.append((np.eye(n_dimention) - gain * self.c.T) * self.covariance_predictor[-1])
-        
+            if each_y != None:
+                gain = self.covariance_predictor[-1] * self.c / ( self.c.T * self.covariance_predictor[-1] * self.c  + sigma_w)
+                self.state_filter.append(self.state_predictor[-1] + gain * ( each_y - self.c.T * self.state_predictor[-1]))
+                self.covariance_filter.append((np.matrix(np.eye(n_dimention)) - gain * self.c.T) * self.covariance_predictor[-1])
+            else:
+                self.state_filter.append(self.state_predictor[-1])
+                self.covariance_filter.append(self.covariance_predictor[-1])
         self.state_filter, self.covariance_filter = self.state_filter[1:], self.covariance_filter[1:]
+ 
         return (self.state_predictor, self.covariance_predictor, self.state_filter, self.covariance_filter, self.y_forecast, self.cov_forecast)
 
-    def logLikelyfood(self, theta, y, x0):
-        sigma_v, sigma_w = np.exp(theta)
-        #sigma_v, sigma_w = (theta)
-        _, _, _, _, y_forecast, cov_forecast = self.filter(y, sigma_v, sigma_w, x0)
+    def logLikelyfood(self, theta, y, x0, context):
         
-        #print np.array(y_forecast)
-        y_forecast = np.array([yf[0,0] for yf in y_forecast])
-        cov_forecast = np.array([yf[0,0] for yf in cov_forecast])
+        #n_cover = (self.b.shape[1] ** 2 - self.b.shape[1]) / 2
+        #sigma_Q = np.matrix(np.diag(np.exp(theta[:self.b.shape[1]])))#+squareform(theta[self.b.shape[1]:self.b.shape[1] + n_cover]))
+        #sigma_w = np.matrix(np.exp(theta[self.b.shape[1]:]))
+        
+        sigma_Q = np.matrix(np.diag(theta[:self.b.shape[1]]))
+        sigma_w = np.matrix(theta[self.b.shape[1]:])
+        parameterA=sigma_w.tolist()
+        #print parameterA
+        sigma_w_arg =  copy.deepcopy(sigma_w.tolist())
+        #print type(sigma_w_arg)
+        _, _, _, _, y_forecast, cov_forecast = kalman.filter(context,y.tolist(),self.A.tolist(),self.b.tolist(),self.c.tolist(),sigma_Q.tolist(),parameterA,x0.tolist())
+        sys.exit(1)
+        #print parameterA
+        #print sigma_w
+        #_, _, _, _, y_forecast, cov_forecast = self.filter(y, sigma_Q, sigma_w, x0)
+        #print np.array([y_forecast[-1][0]])
+        y_forecast = np.array([float(yf[0]) for yf in y_forecast])
+        cov_forecast = np.array([cf[0] for cf in cov_forecast])
         
         r = np.sum(np.log(1. / (2. * math.pi * cov_forecast)) - (y - y_forecast)  ** 2 / (2 * cov_forecast))
-        print "theta:{} loglike:{}".format(theta,r)
+        #print "theta:{} loglike:{}".format(theta,r)
+        
         return -r
 
-    def mle(self, y, x0):
-        r = optimize.minimize(self.logLikelyfood,[1,1],method='Powell',args=(y, x0))
-        return np.exp(r.x) if r.success else False
+    def mle(self, y, x0, method='differential_evolution'):
+        context = kalman.init(y.shape[0],1,x0.shape[0], self.b.shape[1])
+        #n_cover = (self.b.shape[1] ** 2 - self.b.shape[1]) / 2
+        x_min=1e-5
+        x_max=2e+7
         
-    def mleBrute(self, y, x0):
-        rranges = (slice(1, 1000, 100), slice(1, 10000, 100))
-        resbrute = optimize.brute(self.logLikelyfood, rranges, args=(y, x0), full_output=True, finish=optimize.fmin)
-        return resbrute[0]
+        if method == "differential_evolution":
+            r = optimize.differential_evolution(self.logLikelyfood, args=(y, x0, context),bounds=[(x_min,x_max)] * (self.b.shape[1] + 1),init='random',popsize=50)
+        elif method == "slsqp":
+            theta = np.random.random(self.b.shape[1] + 1) * x_max
+            r = optimize.minimize(self.logLikelyfood,theta,method="L-BFGS-B",args=(y, x0),bounds=[(x_min,x_max)] * (self.b.shape[1] + 1))
+            
+        #r = optimize.minimize(self.logLikelyfood,theta,method='Powell',args=(y, x0))
         
-    def smooth(self, y):
-        self.state_smooth, self.covariance_smooth = [0.] * y.shape[0], [0.] * y.shape[0]
+        if r.success == False:
+            raise Exception('optimize failed')
+        return (r.x,r.fun)
+        
+    def smooth(self):
+        
+        self.state_smooth, self.covariance_smooth = [0.] * self.n_sample, [0.] * self.n_sample
         self.state_smooth[-1] = self.state_filter[-1]
         self.covariance_smooth[-1] = self.covariance_filter[-1]
-
-        for idx in range(y.shape[0]-2,-1,-1):
+        
+        for idx in range(self.n_sample-2,-1,-1):
+            #print idx
+            #flag =  True    
+            #try:
+            #    self.covariance_predictor[idx].I
+            #except Exception as e:
+                #print e
+                #print idx
+            #    flag=False
+            #    continue
+            #if flag == False:
+            #    print "flag is false" 
+            #    continue
+            
             gain = self.covariance_filter[idx] * self.A.T * self.covariance_predictor[idx + 1].I
             self.state_smooth[idx] = self.state_filter[idx] + gain * ( self.state_smooth[idx+1] - self.state_predictor[idx+1] )
             self.covariance_smooth[idx] = self.covariance_filter[idx] + gain * ( self.covariance_smooth[idx+1] - self.covariance_predictor[idx+1] ) * gain.T
             
         return np.array(self.state_smooth), np.array(self.covariance_smooth)
 
-    def fit(self, y, x0):
-        sigma_v, sigma_w = self.mle(y, x0)
-        self.filter(y, sigma_v, sigma_w, x0)
-        return self.smooth(y)
+    def fit(self, y, x0=None,repeat=1,mle_method='differential_evolution'):
+        parameters = []
+        
+        if x0 == None:
+            x0 = np.matrix(np.zeros((self.b.shape[0],1)))
+        parameters.append(self.mle(y, x0, method=mle_method))
+        for i in range(repeat):
+            try:
+                parameters.append(self.mle(y, x0, method=mle_method))
+            except Exception as exc:
+                print "Exception Error: {}".format(exc)
+                continue
+            print "i:{0} result:{1}".format(i,parameters[-1])
+        if parameters == []:
+             raise ValueError
+        theta = parameters[np.argmin(map(lambda x: x[1],parameters))][0]
+        
+        #n_cover = (self.b.shape[1] ** 2 - self.b.shape[1]) / 2
+        sigma_Q = np.matrix(np.diag(theta[:self.b.shape[1]]))#+squareform(theta[self.b.shape[1]:self.b.shape[1] + n_cover]))
+        sigma_w = np.matrix(theta[self.b.shape[1]:])
+        
+        self.filter(y, sigma_Q, sigma_w, x0)
+        return self.smooth()
     
     def forecast(self, n_ahead):
         y_forecast = []
@@ -111,16 +206,18 @@ if __name__ == "__main__":
     u"""
     Example
     """
+    
     df1 = pd.read_csv("ts.txt")
-    sample = df1["Sale"].as_matrix()
+    sample = df1[df1["Sale"] > 0]["Sale"].as_matrix()
+    #reverse
+    sample = sample[-1::-1]
+    sample = np.array([[x] for x in sample])
+    lag_acf = acf(sample,nlags=20)
+    print "自己相関関数:{}".format(lag_acf)
 
-    A = np.matrix([[2,-1],[1,0]])
-    b = np.matrix([[1.],[0]])
-    c = np.matrix([[1.],[0]])
-    x0 = np.matrix([[0.],[0.]])
-
-    state_space = PyStateSpace(A,b,c)
-    state_smooth, _ = state_space.fit(sample, x0)
+    model = getModel(1,12)
+    state_space = PyStateSpace(model)
+    state_smooth, _ = state_space.fit(sample,repeat=1,mle_method='differential_evolution')
     sample_predict = state_space.forecast(100)
 
     state_smooth = np.array([ss[0,0] for ss in state_smooth])
@@ -128,6 +225,8 @@ if __name__ == "__main__":
 
     plt.plot(range(state_smooth.shape[0]),state_smooth)
     plt.plot(range(state_smooth.shape[0]),sample)
+    #c = np.matrix([[1.],[0],[0.],[0],[0],[0],[0],[0]])
+    #plt.plot(range(state_smooth.shape[0]),[x[0] for x in state_space.state_filter])
     plt.plot(range(state_smooth.shape[0],state_smooth.shape[0]+len(sample_predict)),sample_predict)
     plt.show()
 
